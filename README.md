f6ed51e19f8251ca0f512334cfb7b8ddd053e10b
baobei-ran <2735827758@qq.com>
2021-01-27 21:34:44 +0800

    搭建webpack5和vue3

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# webpack5.x 和 vue3.x 手搭项目
+
+  - [资源来源](https://mp.weixin.qq.com/s/l5gkwi6LtAEuDwxhKjBxDA)
+
+### 启动
+
+```js
+
+  yarn dev || npm run dev
+
+```
---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">“喂你好，是老张吗？我是叮咚买菜的送菜员，由于您电话打不通，特意在页面里跟您说一声，您菜到家了。” {{msg}}</div>
+</template>
+
+<script>
+export default {
+  setup () {
+    return {
+      msg: '测试手搭webpack5.x项目'
+    }
+  }
+}
+</script>
+<style>
+.home {
+  color: red;
+}
+</style>
